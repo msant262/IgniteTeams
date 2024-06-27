7d7e16a9f16454ec37c1c2565e8f6f54ac377e72
Marcos Santos <msant262@gmail.com>
2024-06-27 17:25:24 +0200

    header

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,11 @@
+import { Header } from "@components/Header";
 import { Container, Title } from "./styles";
 import { StyleSheet, View, Text } from "react-native";
 
 export function Groups(){
     return(
     <Container>
-        <Title>
-            Text
-        </Title>
+        <Header />
     </Container>
     );
 }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -4,8 +4,8 @@ export const Container = styled.View`
     flex: 1;
     background-color: ${({ theme }) => theme.COLORS.GRAY_600};
     padding: 24px;
-    align-items: center;
-    justify-content: center;
+    /* align-items: center; */
+    /* justify-content: center; */
 
 `;
 
