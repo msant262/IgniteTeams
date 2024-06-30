41761921b56d8798aea783d93a2a0cb94bfc4853
Marcos Santos <msant262@gmail.com>
2024-06-30 15:16:49 +0200

    botao

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { Container, Title } from "./styles";
 import { StyleSheet, View, Text, FlatList } from "react-native";
 import { useState } from "react";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups(){
     const [groups, setGroups] = useState<string[]>([]);
@@ -29,6 +30,11 @@ export function Groups(){
             contentContainerStyle={groups.length === 0 && {flex: 1}}
             ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar sua primeira turma?" />}
         />
+
+
+            <Button 
+            title="Cadastrar nova turma"
+            />
 
     </Container>
     );
